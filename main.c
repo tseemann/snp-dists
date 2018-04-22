bd5468fcce51632f062d2814d64be862b3e75dd4
Torsten Seemann <torsten.seemann@gmail.com>
2018-04-22 15:50:25 +1000

    Issue #9 | heap alloc + check N <= MAX_SEQ

--- main.c
+++ main.c
@@ -41,6 +41,9 @@ void show_help(int retcode)
   fprintf(out, "URL\n  %s (%s)\n", GITHUB_URL, AUTHOR);
   exit(retcode);
 }
+
+//------------------------------------------------------------------------
+//char* strshow_help(int retcode)
  
 //------------------------------------------------------------------------
 int main(int argc, char *argv[])
@@ -75,39 +78,33 @@ int main(int argc, char *argv[])
     exit(EXIT_FAILURE);
   }
   
-  // Get the file size so we can estimate # entries from first entry
-  struct stat buf;
-  stat(fasta, &buf);
-  size_t filesize = buf.st_size;
-  if (!quiet) fprintf(stderr, "%s has size %lu bytes\n", fasta, filesize);
-
   // load all the sequences
-  char* seq[MAX_SEQ];
-  char* name[MAX_SEQ];
+  char** seq = (char**) calloc(MAX_SEQ, sizeof(char*));
+  char** name = (char**) calloc(MAX_SEQ, sizeof(char*));
   int l, N=0, L=-1;
   kseq_t* kseq = kseq_init(fp);
 
   while ((l = kseq_read(kseq)) >= 0) {
+    // first sequence
     if (L < 0) { 
       L = l; 
-      int estN = (int) filesize / (kseq->name.l + 1 + kseq->seq.l + 1);
-      if (!quiet) fprintf(stderr, "Estimated alignment to contain %d sequences.\n", estN);
-      if (estN > MAX_SEQ) {
-        fprintf(stderr, "%s can only handle %d sequences at most. Please change MAX_SEQ and recompile.\n", EXENAME, MAX_SEQ);
-        exit(EXIT_FAILURE);
-      }
-      //seq = (char**) calloc( sizeof(char)*kseq->seq.l, estN+10 );
     }
+    // not first sequence - so ensure length matches first one
     if (l != L) {
       fprintf(stderr, "ERROR: sequence #%d '%s' has length %d but expected %d\n", N+1, kseq->name.s, l, L);
       exit(EXIT_FAILURE);
     }
-//        fprintf(stderr, "INFO: %d %s %s\n", N, seq->name.s, seq->seq.s);  // seq.l is length?
+    // have we exceeded the number of sequences we can handle?
+    if (N > MAX_SEQ) {
+      fprintf(stderr, "%s can only handle %d sequences at most. Please change MAX_SEQ and recompile.\n", EXENAME, MAX_SEQ);
+      exit(EXIT_FAILURE);
+    }
+    // save the sequence and name
     seq[N] = (char*) calloc(kseq->seq.l + 1, sizeof(char));
     strcpy(seq[N], kseq->seq.s);
     name[N] = (char*) calloc(kseq->name.l + 1, sizeof(char));
     strcpy(name[N], kseq->name.s);
-//        fprintf(stderr, "INFO: %d %s %s\n", N, name[N], seq[N]);  // seq.l is length?
+    // keep track of how many we have
     N++;
   }
   kseq_destroy(kseq); 
