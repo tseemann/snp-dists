0a1dc7bab342a6447d76dcc1b4ea0c9ad17898fd
Fabian Klötzl <fabian@kloetzl.info>
2018-09-12 11:25:43 +0200

    adjust error message

--- main.c
+++ main.c
@@ -102,7 +102,7 @@ int main(int argc, char *argv[])
     }
     // have we exceeded the number of sequences we can handle?
     if (N >= MAX_SEQ) {
-      fprintf(stderr, "%s can only handle %d sequences at most. Please change MAX_SEQ and recompile.\n", EXENAME, MAX_SEQ);
+      fprintf(stderr, "ERROR: %s can only handle %d sequences at most. Please change MAX_SEQ and recompile.\n", EXENAME, MAX_SEQ);
       exit(EXIT_FAILURE);
     }
     // save the sequence and name
