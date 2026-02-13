8cbc5f1d778a80c435f724a52b3440062d17a936
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2026-02-13 13:14:05 +0100

    Fix typo in help message for -L option

--- main.c
+++ main.c
@@ -47,7 +47,7 @@ void show_help(int retcode)
       "  -a       Count all differences not just [AGTC]\n"
       "  -k       Keep case, don't uppercase all letters\n"
       "  -m       Output MOLTEN instead of TSV\n"
-      "  -L       Ootput lower-trangle only (unique pairs)\n"
+      "  -L       Output lower-triangle only (unique pairs)\n"
       "  -t       Add header to MOLTEN output\n"
       "  -c       Use comma instead of tab in output\n"
       "  -b       Blank top left corner cell\n"
